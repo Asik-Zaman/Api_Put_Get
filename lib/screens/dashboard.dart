e7b4decd7aa5efbe3702c1054a8f8b75d3f3e2e7
Asik <jamanasik@gmail.com>
2022-05-11 10:28:56 +0600

    First Commit

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -4,6 +4,7 @@ import 'package:loginui/constant/sizedbox.dart';
 import 'package:loginui/screens/registerpage.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 
 class Dashboard extends StatefulWidget {
   final String? token;
@@ -25,19 +26,20 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   String? value;
+  var expenseData;
   Future<void> fetchExpense() async {
+    var tokenn = widget.token;
     final response = await http
         .get(Uri.parse('http://mamun.click/api/auth/expense/show'), headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
-      'Authorization': 'Bearer $value',
+      'Authorization': 'Bearer $tokenn',
     });
-    print(value);
-
+    //print(value);
     // print('Token : ${token}');
-    var expenseData = jsonDecode(response.body);
-
-    print(expenseData);
+    expenseData = jsonDecode(response.body);
+    print(expenseData['data'][0]['title']);
+    setState(() {});
   }
 
   /*  Future<void> fetchMenu() async {
@@ -54,62 +56,195 @@ class _DashboardState extends State<Dashboard> {
 
     print(menuData);
   } */
-  Future<void> setTokenValue(token) async {
+  /*  Future<void> setTokenValue(token) async {
     SharedPreferences.setMockInitialValues({});
     final SharedPreferences pref = await SharedPreferences.getInstance();
     pref.setString('tokenValue', token);
-    print(token);
+    //print(token);
   }
 
   void getTokenValue() async {
     final SharedPreferences pref = await SharedPreferences.getInstance();
     value = pref.getString('tokenValue');
-    print('Token is : ' + value.toString());
-  }
+    //print('Token is : ' + value.toString());
+  } */
 /* 
+  */
   @override
   void initState() {
-    setTokenValue(widget.token);
-    getTokenValue();
+    //setTokenValue(widget.token);
+    //getTokenValue();
     fetchExpense();
     //fetchMenu();
     super.initState();
-  } */
+  }
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        elevation: 5,
-        title: Text('User Profile'),
-        centerTitle: true,
-        actions: [
-          OutlinedButton.icon(
-              style: OutlinedButton.styleFrom(primary: Colors.pink),
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => registerpage(),
-                    ),
-                    (route) => false);
-                //Navigator.of(context).pop();
-                /* Navigator.push(context,
+        appBar: AppBar(
+          elevation: 5,
+          title: Text('TRANSACTION HISTORY'),
+          centerTitle: true,
+          leading: Icon(Icons.menu),
+          actions: [
+            OutlinedButton.icon(
+                style: OutlinedButton.styleFrom(primary: Colors.pink),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => registerpage(),
+                      ),
+                      (route) => false);
+                  //Navigator.of(context).pop();
+                  /* Navigator.push(context,
                       MaterialPageRoute(builder: (context) => registerpage())); */
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                size: 18,
-              ),
-              label: Text(
-                'Logout',
-                style: TextStyle(color: Colors.white),
-              ))
-        ],
-      ),
-      body: SafeArea(
+                },
+                icon: Icon(
+                  Icons.exit_to_app,
+                  size: 18,
+                ),
+                label: Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
+        body: expenseData == null
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                height: height,
+                width: width,
+                child: ListView(
+                  children: [
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                expenseData['data'][0]['title'],
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.deepPurple,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              addVerticalSpace(10),
+                              Text(DateFormat('yyyy-MM-dd').format(
+                                  DateTime.parse(
+                                      expenseData['data'][0]['created_at']))),
+                            ],
+                          ),
+                          Text(
+                            '\$' + expenseData['data'][0]['amount'],
+                            style: TextStyle(fontSize: 25, color: Colors.pink),
+                          ),
+                        ],
+                      ),
+                    )),
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                expenseData['data'][0]['title'],
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.deepPurple,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              addVerticalSpace(10),
+                              Text(DateFormat('yyyy-MM-dd').format(
+                                  DateTime.parse(
+                                      expenseData['data'][0]['created_at']))),
+                            ],
+                          ),
+                          Text(
+                            '\$' + expenseData['data'][0]['amount'],
+                            style: TextStyle(fontSize: 25, color: Colors.pink),
+                          ),
+                        ],
+                      ),
+                    )),
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                expenseData['data'][0]['title'],
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.deepPurple,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              addVerticalSpace(10),
+                              Text(DateFormat('yyyy-MM-dd').format(
+                                  DateTime.parse(
+                                      expenseData['data'][0]['created_at']))),
+                            ],
+                          ),
+                          Text(
+                            '\$' + expenseData['data'][0]['amount'],
+                            style: TextStyle(fontSize: 25, color: Colors.pink),
+                          ),
+                        ],
+                      ),
+                    )),
+                    Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                expenseData['data'][0]['title'],
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.deepPurple,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              addVerticalSpace(10),
+                              Text(DateFormat('yyyy-MM-dd').format(
+                                  DateTime.parse(
+                                      expenseData['data'][0]['created_at']))),
+                            ],
+                          ),
+                          Text(
+                            '\$ ' + expenseData['data'][0]['amount'],
+                            style: TextStyle(fontSize: 25, color: Colors.pink),
+                          ),
+                        ],
+                      ),
+                    )),
+                  ],
+                ),
+              ));
+  }
+}
+/*SafeArea(
         child: Container(
           height: height,
           width: width,
@@ -154,7 +289,7 @@ class _DashboardState extends State<Dashboard> {
                   style: TextStyle(fontSize: 20),
                 ),
               ),
-              addVerticalSpace(15),
+              /*  addVerticalSpace(15),
               ElevatedButton(
                   onPressed: () {
                     setTokenValue(widget.token);
@@ -169,11 +304,8 @@ class _DashboardState extends State<Dashboard> {
                   onPressed: () {
                     fetchExpense();
                   },
-                  child: Text('Fetch Data'))
+                  child: Text('Fetch Data')) */
             ],
           ),
         ),
-      ),
-    );
-  }
-}
+      ),*/
